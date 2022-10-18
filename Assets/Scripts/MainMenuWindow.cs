0972d607d1ffb472f9940ad3528c11989f08417e
Carl Meyer <carl@ukiyo-studio.co.uk>
2022-10-18 17:49:09 +0100

    Main Menu Button Functions

--- Assets/Scripts/MainMenuWindow.cs
+++ Assets/Scripts/MainMenuWindow.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.Tracing;
 using CodeMonkey.Utils;
 using UnityEngine;
 
@@ -5,13 +6,17 @@ public class MainMenuWindow : MonoBehaviour
 {
     private void Awake()
     {
-        transform.Find("PlayButton").GetComponent<Button_UI>().ClickFunc = () =>
-        {
-            Debug.Log("Play Clicked!");
-            Loader.Load(Loader.Scene.GameNewTest);
-        };
         transform.Find("PlayButton").GetComponent<Button_UI>().AddButtonSounds();
-        transform.Find("QuitButton").GetComponent<Button_UI>().ClickFunc = Application.Quit;
         transform.Find("QuitButton").GetComponent<Button_UI>().AddButtonSounds();
+    }
+
+    public void OnPlay()
+    {
+        Loader.Load(Loader.Scene.GameNewTest);
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
     }
 }
